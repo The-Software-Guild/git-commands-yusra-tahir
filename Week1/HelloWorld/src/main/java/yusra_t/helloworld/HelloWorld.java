26f6b7323c83eaba2b235d41288974fc2371dccb
Yusra Tahir <yusra-tahir@hotmail.co.uk>
2022-10-11 15:37:47 +0100

    Hello World in NetBeans

---
+++ Week1/HelloWorld/src/main/java/yusra_t/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Yusra
+ *email: yusra-tahir@hotmail.co.uk
+ *date: 11-10-2022
+ *purpose: My first Hello World program in an IDE
+ */
+
+package yusra_t.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Yusra!");
+    }
+}
