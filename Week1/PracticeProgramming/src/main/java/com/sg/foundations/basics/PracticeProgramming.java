f16ad130d985153226ef5f553dec1165bb705cfd
Yusra Tahir <yusra-tahir@hotmail.co.uk>
2022-10-12 12:06:42 +0100

    Exercise: Practice Programming #1 - Basics

---
+++ Week1/PracticeProgramming/src/main/java/com/sg/foundations/basics/PracticeProgramming.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Yusra
+ */
+package com.sg.foundations.basics;
+
+public class PracticeProgramming {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Week1/PracticeProgramming/src/main/java/com/sg/foundations/basics/ProjectGutenberg.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Yusra
+ * email: yusra-tahir@hotmail.co.uk
+ * date: 12-10-2022
+ */
+package com.sg.foundations.basics;
+
+public class ProjectGutenberg {
+    public static void main(String[] args) {
+    
+        System.out.print("Did you know that in 1440 (or thereabouts),");
+        System.out.print("Johannes Gutenberg invented the printing press?");
+        System.out.print("He started out as a goldsmith!");
+        System.out.print("His invention made it easy to print and");
+        System.out.print("distribute books to anyone who wanted one.");
+        System.out.print("We are like a modern Gutenberg,");
+        System.out.print("printing vast amounts to the waiting console with ease.");
+    }
+        
+}
