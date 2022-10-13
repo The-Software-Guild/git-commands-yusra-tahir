e827492689ec128d8d56c3a0204e8f197b025a80
Yusra Tahir <yusra-tahir@hotmail.co.uk>
2022-10-13 11:00:37 +0100

    updated WindowMaster using Methods

--- Week1/WindowMaster/src/main/java/yusra_t/windowmaster/WindowMaster.java
+++ Week1/WindowMaster/src/main/java/yusra_t/windowmaster/WindowMaster.java
@@ -3,7 +3,6 @@
  * email: yusra-tahir@hotmail.co.uk
  * date: 12-10-2022
  */
-
 package yusra_t.windowmaster;
 
 import java.util.Scanner;
@@ -12,59 +11,48 @@ public class WindowMaster {
 
     public static void main(String[] args) {
         // Declare variables that will be needed
-        
-        float height;
-        float width;
-        float windowCost;
-        float trimCost;
-        float windowNumber;
-        
-        float areaOfWindow;
-        float perimeterOfWindow;
-        float cost;
-        
-        String stringHeight;
-        String stringWidth;
-        String costOfWindow;
-        String costOfTrim;
-        String numberOfWindows;
-        
+
+        float height, width, windowCost, trimCost, windowNumber, areaOfWindow, perimeterOfWindow, cost;
+
         // Declare and initilaise the Scanner
-        Scanner myScanner = new Scanner(System.in);
-        
+        Scanner sc = new Scanner(System.in);
+
         // User input 
-        System.out.println("Please enter the number of windows:");
-        numberOfWindows = myScanner.nextLine();
-        System.out.println("Please enter window height:");
-        stringHeight = myScanner.nextLine();
-        System.out.println("Please eneter window width:");
-        stringWidth = myScanner.nextLine();
-        System.out.println("Please enter cost of  window:");
-        costOfWindow = myScanner.nextLine();
-        System.out.println("Please enter cose of trim:");
-        costOfTrim = myScanner.nextLine();
-        
-        // Convert String values of height and width to float values 
-        height = Float.parseFloat(stringHeight);
-        width = Float.parseFloat(stringWidth);
-        windowCost = Float.parseFloat(costOfWindow);
-        trimCost = Float.parseFloat(costOfTrim);
-        windowNumber = Float.parseFloat(numberOfWindows);
-        
+        height = readValue("Please enter window height:");
+        width = readValue("Please eneter window width:");
+        windowCost = readValue("Please enter cost of  window:");
+        trimCost = readValue("Please enter cose of trim:");
+        windowNumber = readValue("Please enter the number of windows:");
+
         // Calculate the area of the window              
         areaOfWindow = height * width;
-        
+
         // Calculate the perimeter of the window   
         perimeterOfWindow = 2 * (height + width);
-        
+
         // Calculate total cost 
         cost = (windowNumber * (windowCost * areaOfWindow) + (trimCost * perimeterOfWindow));
-        
+
         // Display the results to the user
-        System.out.println("Window height = " + stringHeight);
-        System.out.println("Window width = " + stringWidth);
+        System.out.println("Window height = " + height);
+        System.out.println("Window width = " + width);
         System.out.println("Window area = " + areaOfWindow);
         System.out.println("Window perimeter = " + perimeterOfWindow);
         System.out.println("Total Cost =  " + cost);
+    }
+
+    public static float readValue(String prompt) {
+        // Declaring a scanner variable 
+        Scanner sc = new Scanner(System.in);
+
+        // Print question to console for user input
+        System.out.println(prompt);
+
+        // Read the input given by the user
+        String input = sc.nextLine();
+
+        // Convert the string received from user to a float
+        float floatVal = Float.parseFloat(input);
+        return floatVal;
     }
 }
